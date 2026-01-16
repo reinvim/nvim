24bbefaca59b1677bf52a5d68dda11e32e1f259a
ReinVim <reinvim@gmail.com>
2026-01-16 21:36:49 +0800

    Added LazyGit for easier git management

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,24 @@
+return {
+	"kdheepak/lazygit.nvim",
+	lazy = true,
+	cmd = {
+		"LazyGit",
+		"LazyGitConfig",
+		"LazyGitCurrentFile",
+		"LazyGitFilter",
+		"LazyGitFilterCurrentFile",
+	},
+	-- optional for floating window border decoration
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	-- setting the keybinding for LazyGit with 'keys' is recommended in
+	-- order to load the plugin when the command is run for the first time
+	keys = {
+		{ "<leader>g", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		{ "<leader>gs", ":LazyGit status<CR>", desc = "LazyGit status" },
+		{ "<leader>gc", ":LazyGit commit<CR>", desc = "LazyGit commit" },
+		{ "<leader>gp", ":LazyGit push<CR>", desc = "LazyGit push" },
+		{ "<leader>q", ":q<CR>", desc = "Quit" },
+	},
+}
